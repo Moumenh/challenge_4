f3b7fe6cf99a34838e11f7763972a89220484c9a
Moumenh <mohanbali94@gmail.com>
2020-11-05 11:08:54 +0200

    webpack configurations

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <h1> Render This</h1>
+    </div>
+)
 
 
+
+ReactDOM.render(<App />, document.getElementById('app'));
