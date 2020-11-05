a38f53ac5f6fd39211ca2cd335cf8edc7b57ad57
Moumenh <mohanbali94@gmail.com>
2020-11-05 11:51:16 +0200

    loading react app files to serv

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 
+
 const app = express()
+
+app.use(express.urlencoded({ extended: true })) // to parse the data
+app.use(express.json()) // to make the content header     application/json
+
+app.use(express.static(__dirname + '/./public'));
 
 
 app.listen(3000, () =>{
